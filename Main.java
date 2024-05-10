755f148aba9476028ae27481c6ffb8fa94d25537
denchentz <160261154+denchentz@users.noreply.github.com>
2024-05-10 14:00:23 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package edu.bhcc.semester_project;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 480;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Spaceship Game");
+		frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Game game = new Game();
+        frame.add(game);
+        frame.pack();
+        game.requestFocus();
+        frame.setVisible(true);
+    }
+}
